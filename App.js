78fcb8fb93226e122a3ea820aa21ac00879eeb39
disilvap <diego.silva.pr@outlook.com>
2020-07-03 21:31:26 -0500

    useEffect

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, } from 'react-native';
 
+// useEffect sirve para realizar llamados asíncronos, por ejemplo a una API o cualquier cosa que genere efectos
+
 export default function App(){
-// Los hooks retornan un arreglo
-// El primer elemento del arreglo, es un estado, el segundo elemento, es una función que se utilizará para actualizar el valor del estado
+  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  // // Cada vez que se monte el componente o se haya actualizado, llamará a la función que tiene dentro (setTimeout)
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false)
+  //   }, 2000);
+  // })
 
-// La primera vez que se inicia la variable count, vale 0
-const [count, setCount] = useState(0)
+  // // Sólo ejecutará la función la primera vez que el componente se haya montado
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+  }, [])
 
+  // // Sólo ejecutará la función cuando el dato pasado como argumento, cambie
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false)
+  //   }, 2000);
+  // }, [dato])
 
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hooks - useState</Text>
-      <Text onPress={()=> setCount(count+1)} style={styles.text}>{count}</Text>
+      <Text onPress={()=> setCount(count+1)} style={styles.text}>{loading ? 'Cargando...' : count}</Text>
     </View>   
   );
 }
