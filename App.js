0aef5cdbef531d9715dedbc1a58b45129df36a63
disilvap <diego.silva.pr@outlook.com>
2020-07-03 21:19:38 -0500

    useReducer

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, View, Text, } from 'react-native';
 
+const initialState = {
+  count: 0
+}
+
+// Recibimos un estado y una acción
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'incrementar': {
+      return { count: state.count + 1}
+    }
+    case 'decrementar': {
+      return { count: state.count -  1}
+    }
+    // si no hay ninguna acción, se regresa el estado original que recibimos
+    default: {
+      return state
+    }
+  }
+}
+
 export default function App(){
-// Los hooks retornan un arreglo
-// El primer elemento del arreglo, es un estado, el segundo elemento, es una función que se utilizará para actualizar el valor del estado
-
-// La primera vez que se inicia la variable count, vale 0
-const [count, setCount] = useState(0)
-
-
-
+  // state, la primera vez, tendrá el valor de 0
+  // dispatch es la función que nos permitirá despachar acciones como 'incrementar' y 'decrementar'
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hooks - useState</Text>
-      <Text onPress={()=> setCount(count+1)} style={styles.text}>{count}</Text>
+      <Text style={styles.text}>Hooks - useReducer</Text>
+      <Text style={styles.counter} onPress={() => dispatch({ type: 'incrementar'})}>+</Text>
+      <Text style={styles.counter} >{state.count}</Text>
+      <Text style={styles.counter} onPress={() => dispatch({ type: 'decrementar'})}>-</Text>
     </View>   
   );
 }
@@ -27,5 +44,8 @@ const styles = StyleSheet.create({
   },
   text:{
     fontSize: 24
+  },
+  counter:{
+    fontSize: 40
   }
 })
